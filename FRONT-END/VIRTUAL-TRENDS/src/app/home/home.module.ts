1a8e7d17dda84d01130dd2777ca8207237fa15b0
Carla Antonini <63260839+Gaghyta@users.noreply.github.com>
2023-05-20 14:26:29 -0300

    Revert "Feature categories products"

--- FRONT-END/VIRTUAL-TRENDS/src/app/home/home.module.ts
+++ FRONT-END/VIRTUAL-TRENDS/src/app/home/home.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SectionsDescriptionsComponent } from './sections-descriptions/sections-descriptions.component';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
 import { HomeImagesComponent } from './home-images/home-images.component';
-import { SectionCategoriesProductsComponent } from './section-categories-products/section-categories-products.component';
 
 
 @NgModule({
-  declarations: [DashboardHomeComponent,SectionsDescriptionsComponent,SectionCategoriesProductsComponent,HomeImagesComponent],
+  declarations: [DashboardHomeComponent,SectionsDescriptionsComponent, HomeImagesComponent],
   imports: [
     CommonModule
   ]
